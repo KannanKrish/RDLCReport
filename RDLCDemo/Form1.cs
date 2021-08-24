ff9e0b7e5e5c29fd3b36edececfee352d02a6ff2
Kannan Chithambaranathan <ckannan759153@gmail.com>
2021-08-24 16:40:28 +0530

    Initial Project

---
+++ RDLCDemo/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+
+namespace RDLCDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Setting Report Viewer RDLC File
+            // Using Embedded Resources
+            //reportViewer1.LocalReport.ReportEmbeddedResource = GetType().Namespace + ".DemoReport.rdlc";
+
+            //using File Path. For that RDLC file need to copy inside executable file.
+            reportViewer1.LocalReport.ReportPath = "DemoReport.rdlc";
+
+            //Setting Parameters for RDLC Report
+            reportViewer1.LocalReport.SetParameters(new ReportParameter("Header", "My Header"));
+            reportViewer1.LocalReport.SetParameters(new ReportParameter("Footer", "My Footer"));
+
+            // Loading DataSource in Report Viewer
+            reportViewer1.LocalReport.DataSources.Add(new ReportDataSource("StudentDataSet",Student.GetStudents()));
+            
+            // Refreshing the Report Viewer
+            reportViewer1.LocalReport.Refresh();
+            reportViewer1.RefreshReport();
+        }
+    }
+}
---
+++ RDLCDemo/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace RDLCDemo
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public Student(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+
+        public static List<Student> GetStudents()
+        {
+            return new List<Student>()
+            {
+                new Student("Kannan", 20, "Nagercoil"),
+                new Student("Karthi", 20, "Nagercoil"),
+                new Student("Rajan", 20, "Nagercoil")
+            };
+        }
+    }
+}
